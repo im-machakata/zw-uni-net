157b297a2a6e9a915575ecbbe1dabdc0a7aeb4f2
Isaac Machakata <isaac@caasi.co.zw>
2024-04-08 02:44:38 +0200

    refactor: add global page layouts and templates

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="author" content="Knowledge">
+    <meta name="description" content="Find your next university with Unimap, e-map for seniors">
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/static/css/bootstrap.min.css?version=5.3.3">
+    <link rel="stylesheet" href="/static/css/style.css">
+    <title>@yield('title','Uni Map')</title>
+</head>
+
+<body>
+
+    <main>
+        @extends('layouts.nav')
+        @yield('content')
+        @extends('layouts.footer')
+    </main>
+</body>
+
+<script src="/static/js/jquery.3.7.1.min.js" async defer></script>
+<script src="/static/js/bootstrap.bundle.min.js?version=5.3.3" async defer></script>
+<script src="/static/js/app.js" async defer></script>
+
+</html>
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg bg-white fixed-top border-bottom border-dark border-4" aria-label="Thirteenth navbar example">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample11" aria-controls="navbarsExample11" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse d-lg-flex" id="navbarsExample11">
+            <a class="navbar-brand fw-bold col-lg-3 me-0" href="#">Unimap</a>
+            <ul class="navbar-nav col-lg-6 justify-content-lg-center">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Universities</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Programs</a>
+                </li>
+            </ul>
+            <div class="d-lg-flex col-lg-3 justify-content-lg-end">
+                <a class="btn btn-outline-dark" href="#">Login</a>
+                <a class="btn btn-dark ms-lg-2" href="#">Register</a>
+            </div>
+        </div>
+    </div>
+</nav>
