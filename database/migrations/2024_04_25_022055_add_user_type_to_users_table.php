0be7a64f10e09b62c0445d0102d38f0f60ebb47e
Machakata Isaac <isaac@caasi.co.zw>
2024-04-25 02:22:54 +0000

    refactor: add user type column

---
+++ database/migrations/2024_04_25_022055_add_user_type_to_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // add user type column [student, university]
+            $table->enum('type', ['student', 'university'])->default('student');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('type');
+        });
+    }
+};
