4a75225ce25736144f9b2541e873a1430fa48eae
Isaac Machakata <isaac@caasi.co.zw>
2024-05-02 18:52:42 +0200

    refactor: format migration middleware

--- app/Http/Controllers/MigrationController.php
+++ app/Http/Controllers/MigrationController.php
@@ -7,12 +7,14 @@ use Illuminate\Support\Facades\Artisan;
 
 class MigrationController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         if ($request->has('force')) {
             Artisan::call('migrate:fresh');
             Artisan::call('db:seed');
         } else {
             Artisan::call('migrate');
         }
-        return 'Done';}
+        return 'Done';
+    }
 }
