cf32362f888ed43f9b3f1929eb17132fc518cf16
Isaac Machakata <isaac@caasi.co.zw>
2024-05-05 22:53:02 +0200

    style: auth forms

--- resources/views/account/login.blade.php
+++ resources/views/account/login.blade.php
@@ -1,39 +1,35 @@
 @extends('layouts.app')
 @section('title','Unimap Login')
 @section('content')
-<section class="bg-dark" style="background: url(/static/images/class.jpg);">
-    <div class="container-fluid" style="background: linear-gradient(rgba(0,0,0,.8),rgba(0,0,0,.6))">
-        <div class="cover text-center px-lg-5 px-3">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-6 col-lg-4">
-                        <div class="card p-0">
-                            <div class="card-body">
-                                <h1 class="text-center">Login</h1>
-                                <p class="text-muted">Welcome back!</p>
-                                @include('layouts.error')
-                                <form action="/login" class="signin-form" method="post">
-                                    @csrf
-                                    <div class="form-group form-floating mb-3">
-                                        <input type="email" name="email" id="email" class="form-control" placeholder="Email" placeholder="email@gmail.com" required>
-                                        <label for="email">Email</label>
-                                    </div>
-                                    <div class="form-group form-floating mb-3">
-                                        <input id="password-field" name="password" type="password" class="form-control" placeholder="Password" required>
-                                        <label for="password">Password</label>
-                                        <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
-                                    </div>
-                                    <div class="form-group">
-                                        <button type="submit" class="form-control btn btn-lg btn-primary submit px-3 mb-3">Sign In</button>
-                                    </div>
-                                    <div class="form-group d-md-flex">
-                                        <div class="text-md-right">
-                                            <!-- <a href="#">Forgot Password</a> -->
-                                        </div>
-                                    </div>
-                                </form>
+<section class="container-fluid">
+    <div class="container-fluid cover">
+        <div class="row container-fluid justify-content-center py-5">
+            <div class="col-md-6 col-lg-4">
+                <div class="card p-0 mt-5">
+                    <div class="card-body">
+                        <h1 class="text-center">Login</h1>
+                        <p class="text-muted text-center">Welcome back!</p>
+                        @include('layouts.error')
+                        <form action="/login" class="signin-form" method="post">
+                            @csrf
+                            <div class="form-group form-floating mb-3">
+                                <input type="email" name="email" id="email" class="form-control" placeholder="Email" placeholder="email@gmail.com" required>
+                                <label for="email">Email</label>
                             </div>
-                        </div>
+                            <div class="form-group form-floating mb-3">
+                                <input id="password-field" name="password" type="password" class="form-control" placeholder="Password" required>
+                                <label for="password">Password</label>
+                                <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="form-control btn btn-lg btn-dark submit px-3 mb-3">Sign In</button>
+                            </div>
+                            <div class="form-group d-md-flex">
+                                <div class="text-md-right">
+                                    <!-- <a href="#">Forgot Password</a> -->
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
--- resources/views/account/register.blade.php
+++ resources/views/account/register.blade.php
@@ -1,63 +1,59 @@
 @extends('layouts.app')
 @section('title','Unimap Register Account')
 @section('content')
-<section class="bg-dark" style="background: url(/static/images/class.jpg);">
-    <div class="container-fluid" style="background: linear-gradient(rgba(0,0,0,.8),rgba(0,0,0,.6))">
-        <div class="cover text-center px-lg-5 px-3">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6 py-4">
-                        <div class="card mt-5">
-                            <div class="card-body">
-                                <h1 class="text-center">Register</h1>
-                                <p class="text-muted">Join our community</p>
-                                @include('layouts.error')
-                                <form action="/register" class="signin-form" method="post">
-                                    @csrf
-                                    <div class="row">
-                                        <div class="col-lg-6">
-                                            <div class="form-group form-floating mb-3">
-                                                <input type="text" name="name" id="name" class="form-control" placeholder="John Doe" required>
-                                                <label for="name">Name</label>
-                                            </div>
-                                        </div>
-                                        <div class="col-lg-6">
-                                            <div class="form-group form-floating mb-3">
-                                                <input type="email" name="email" id="email" class="form-control" placeholder="email@gmail.com" required>
-                                                <label for="email">Email</label>
-                                            </div>
-                                        </div>
-                                        <div class="col-12">
-                                            <div class="form-group form-floating mb-3">
-                                                <select name="type" id="type" class="form-select" required>
-                                                    <option value="student" selected>Student</option>
-                                                    <option value="university">University</option>
-                                                </select>
-                                                <label for="type">User Type</label>
-                                            </div>
-                                        </div>
-                                    </div>
+<section class="container-fluid">
+    <div class="container-fluid">
+        <div class="row justify-content-center container-fluid py-5">
+            <div class="col-md-6 col-lg-4 py-4">
+                <div class="card mt-5">
+                    <div class="card-body">
+                        <h1 class="text-center">Register</h1>
+                        <p class="text-muted text-center">Join our community</p>
+                        @include('layouts.error')
+                        <form action="/register" class="signin-form" method="post">
+                            @csrf
+                            <div class="row">
+                                <div class="col-lg-6">
                                     <div class="form-group form-floating mb-3">
-                                        <input id="password-field" name="password" type="password" class="form-control" placeholder="Password" required>
-                                        <label for="password">Password</label>
-                                        <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+                                        <input type="text" name="name" id="name" class="form-control" placeholder="John Doe" required>
+                                        <label for="name">Name</label>
                                     </div>
+                                </div>
+                                <div class="col-lg-6">
                                     <div class="form-group form-floating mb-3">
-                                        <input id="confirm-password-field" name="password_confirmation" type="password" class="form-control" placeholder="Password" required>
-                                        <label for="confirm-password">Confirm Password</label>
-                                        <span toggle="#confirm-password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+                                        <input type="email" name="email" id="email" class="form-control" placeholder="email@gmail.com" required>
+                                        <label for="email">Email</label>
                                     </div>
-                                    <div class="form-group">
-                                        <button type="submit" class="form-control btn btn-lg btn-primary submit px-3 mb-3">Sign Up</button>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group form-floating mb-3">
+                                        <select name="type" id="type" class="form-select" required>
+                                            <option value="student" selected>Student</option>
+                                            <option value="university">University</option>
+                                        </select>
+                                        <label for="type">User Type</label>
                                     </div>
-                                    <div class="form-group d-md-flex">
-                                        <div class="text-md-right">
-                                            <!-- <a href="#">Forgot Password</a> -->
-                                        </div>
-                                    </div>
-                                </form>
+                                </div>
                             </div>
-                        </div>
+                            <div class="form-group form-floating mb-3">
+                                <input id="password-field" name="password" type="password" class="form-control" placeholder="Password" required>
+                                <label for="password">Password</label>
+                                <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+                            </div>
+                            <div class="form-group form-floating mb-3">
+                                <input id="confirm-password-field" name="password_confirmation" type="password" class="form-control" placeholder="Password" required>
+                                <label for="confirm-password">Confirm Password</label>
+                                <span toggle="#confirm-password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="form-control btn btn-lg btn-dark submit px-3 mb-3">Sign Up</button>
+                            </div>
+                            <div class="form-group d-md-flex">
+                                <div class="text-md-right">
+                                    <!-- <a href="#">Forgot Password</a> -->
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
