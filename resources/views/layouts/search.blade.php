99259d6c693fc2f72635a7ddd83db2ec4572ba76
Isaac Machakata <isaac@caasi.co.zw>
2024-05-08 08:15:18 +0200

    refactor: input search placeholder

--- resources/views/layouts/search.blade.php
+++ resources/views/layouts/search.blade.php
@@ -1,7 +1,7 @@
 <form action="/search" method="get" class="mt-2">
     <div class="row bg-light py-2 round-off">
         <div class="col-8 col-lg-10">
-            <input type="search" name="q" class="form-control form-control-lg w-100 border-0" placeholder="Which Program(s) Are You Interested In? " value="{{ $query ?? '' }}" required>
+            <input type="search" name="q" class="form-control form-control-lg w-100 border-0" placeholder="Search by program, institution or any other keywords..." value="{{ $query ?? '' }}" required>
         </div>
         <div class="col-4 col-lg-2">
             <button type="submit" class="btn btn-dark w-100 btn-lg rounded-2 text-uppercase">find</button>
