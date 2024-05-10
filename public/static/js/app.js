1ca66939ad60d84c40e9edb9918098d50ef6f1fe
Isaac Machakata <isaac@caasi.co.zw>
2024-05-10 07:59:42 +0200

    feat: prompt before deletion of institution

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -1,3 +1,12 @@
 $(function () {
-    // silence is gloden
+    $('#deleteUniversity').on('click', function (e) {
+        const confirmsDeletion = confirm('Are you sure you want to delete this University? This can not be undone');
+
+        // if user clicks on no
+        // prevent university deletion
+        if (!confirmsDeletion) {
+            e.preventDefault();
+        }
+        return;
+    });
 });
