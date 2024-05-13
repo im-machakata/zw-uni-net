8ec680f7c0d441bc099256c6dcef788db3aa63b3
Isaac Machakata <isaac@caasi.co.zw>
2024-05-13 06:18:18 +0200

    refactor: use fontawesome from cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,9 @@
 <meta name="description" content="Find your next university with Unimap, e-map for seniors">
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="/static/css/fas/all.css?version=6.3.0-alpha">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+    integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="/static/css/bootstrap.min.css?version=5.3.3">
 <link rel="stylesheet" href="/static/css/style.css?version={{ filemtime(public_path('static/css/style.css')) }}">
 <title>@yield('title','Uni Map')</title>
