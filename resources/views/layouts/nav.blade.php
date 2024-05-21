f6a0d8413c7a23d07f83820f4ac1a4f33befccfb
Isaac Machakata <isaac@caasi.co.zw>
2024-05-21 05:22:41 +0200

    feat: add feedback and messaes url  to menu

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -13,7 +13,13 @@
                 <li class="nav-item">
                     <a class="nav-link" href="/universities">Universities</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/messages">Messages</a>
+                </li>
                 @endif
+                <li class="nav-item">
+                    <a class="nav-link" href="/contact-us">Contact Us</a>
+                </li>
             </ul>
             @if(!session()->get('user'))
             <div class="d-lg-flex col-lg-3 justify-content-lg-end">
