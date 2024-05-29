0da1608a16b588f5033055895e2bdab49a0d14a9
Machakata Isaac <isaac@caasi.co.zw>
2024-05-29 03:42:04 +0000

    refactor: include nav & footer correctly

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,25 +5,26 @@
 <meta name="description" content="Find your next university with Unimap, e-map for seniors">
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-    integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="/static/css/bootstrap.min.css?version=5.3.3">
+<link rel="stylesheet" href="/static/css/bootstrap-tagsinput.css?version=f9a93ca">
 <link rel="stylesheet" href="/static/css/style.css?version={{ filemtime(public_path('static/css/style.css')) }}">
+<livewire:styles />
 <title>@yield('title','Uni Map')</title>
 </head>
 
 <body>
 
     <main>
-        @extends('layouts.nav')
+        @include('layouts.nav')
         @yield('content')
-        @extends('layouts.footer')
+        @include('layouts.footer')
     </main>
+    <script src="/static/js/jquery.3.7.1.min.js"></script>
+    <script src="/static/js/bootstrap.bundle.min.js?version=5.3.3"></script>
+    <script src="/static/js/bootstrap-tagsinput.min.js?version=f9a93ca"></script>
+    <script src="/static/js/app.js"></script>
+    <livewire:scripts />
 </body>
-
-<script src="/static/js/jquery.3.7.1.min.js" defer></script>
-<script src="/static/js/bootstrap.bundle.min.js?version=5.3.3" defer></script>
-<script src="/static/js/app.js" defer></script>
 
 </html>
